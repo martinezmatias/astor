e97e906470b48b99a0ddefd61803e6fd2278f43d
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-09-15 10:43:49 +0200

    add cases

--- src/test/java/fr/inria/astor/test/D4JWorkflowTestSingle.java
+++ src/test/java/fr/inria/astor/test/D4JWorkflowTestSingle.java
@@ -13,13 +13,93 @@ public class D4JWorkflowTestSingle {
 	public String bugidParametrized;
 
 	@Test
+	public void testMath2() throws Exception {
+		D4JWorkflowTest.runComplete("Math2", "");
+	}
+
+	@Test
+	public void testMath5() throws Exception {
+		D4JWorkflowTest.runComplete("Math5", "");
+	}
+
+	@Test
+	public void testMath8() throws Exception {
+		D4JWorkflowTest.runComplete("Math8", "");
+	}
+
+	@Test
+	public void testMath28() throws Exception {
+		D4JWorkflowTest.runComplete("Math28", "");
+	}
+
+	@Test
+	public void testMath40() throws Exception {
+		D4JWorkflowTest.runComplete("Math40", "");
+	}
+
+	@Test
+	public void testMath49() throws Exception {
+		D4JWorkflowTest.runComplete("Math49", "");
+	}
+
+	@Test
+	public void testMath50() throws Exception {
+		D4JWorkflowTest.runComplete("Math50", "");
+	}
+
+	@Test
+	public void testMath53() throws Exception {
+		D4JWorkflowTest.runComplete("Math53", "");
+	}
+
+	@Test
 	public void testMath70() throws Exception {
 		D4JWorkflowTest.runComplete("Math70", "");
 	}
 
 	@Test
+	public void testMath71() throws Exception {
+		D4JWorkflowTest.runComplete("Math71", "");
+	}
+
+	@Test
+	public void testMath73() throws Exception {
+		D4JWorkflowTest.runComplete("Math73", "");
+	}
+
+	@Test
+	public void testMath78() throws Exception {
+		D4JWorkflowTest.runComplete("Math78", "");
+	}
+
+	@Test
+	public void testMath80() throws Exception {
+		D4JWorkflowTest.runComplete("Math80", "");
+	}
+
+	@Test
 	public void testMath81() throws Exception {
 		D4JWorkflowTest.runComplete("Math81", "");
+	}
+
+	@Test
+	public void testMath82() throws Exception {
+		D4JWorkflowTest.runComplete("Math82", "");
+	}
+
+	@Test
+	public void testMath84() throws Exception {
+		D4JWorkflowTest.runComplete("Math84", "");
+	}
+
+	@Test
+	public void testMath85() throws Exception {
+		D4JWorkflowTest.runComplete("Math85", "");
+	}
+
+	@Test
+	public void testMath95() throws Exception {
+		D4JWorkflowTest.runComplete("Math95", "");
 	}
 
 }
