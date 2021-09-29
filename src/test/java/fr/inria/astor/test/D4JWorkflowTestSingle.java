49102eb920a7e7b69194b9d654887b8535c33c38
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-09-29 08:09:16 +0200

    parameters

--- src/test/java/fr/inria/astor/test/D4JWorkflowTestSingle.java
+++ src/test/java/fr/inria/astor/test/D4JWorkflowTestSingle.java
@@ -205,7 +205,8 @@ public class D4JWorkflowTestSingle {
 	public void testMath40JKali() throws Exception {
 		CommandSummary cs = new CommandSummary();
 		cs.command.putIfAbsent("-flthreshold", "0");
-		cs.command.put("-logtestexecution", "true");
+		// cs.command.put("-logtestexecution", "true");
+		cs.command.put("-parameters", "logtestexecution:true");
 
 		runComplete("Math40", "", "jKali", 30, cs);
 	}
